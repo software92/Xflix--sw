9b527f825a542581d6a5689708adba0ffb2b35a5
jsw <csw9201@naver.com>
2024-12-17 11:32:46 +0900

    Xflix init && tailwindcss init

---
+++ README.md
@@ -0,0 +1,7 @@
+# Xflix
+
+영화, 드라마 등 컨텐츠 내역을 확인할 수 있는 어플리케이션
+
+## Dependencies
+
+- tailwindcss
---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import Item from "./Item";
+
+function App() {
+  return (
+    <>
+      <h1 className="text-3xl">Xflix app</h1>
+      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+      <p className="underline">Hello world!</p>
+      <Item />
+    </>
+  );
+}
+
+export default App;
