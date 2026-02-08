ee6b3edc5695661cd5af74b3334a7d120524df6c
jsw <csw9201@naver.com>
2026-02-08 16:54:56 +0900

    feat, style: footer 컴포넌트 생성 및 스타일

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import { Outlet } from 'react-router'
 import Header from './components/layout/Header'
+import Footer from './components/layout/Footer'
 
 function App() {
   return (
     <main className='min-h-screen bg-black'>
       <Header />
       <Outlet />
+      <Footer />
     </main>
   )
 }
