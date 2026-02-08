f80ff65615da8512adf10ddea0c6ea5875df917a
jsw <csw9201@naver.com>
2026-02-08 15:06:44 +0900

    style: 영화 슬라이드 카드 스타일

--- src/components/MoviesRow.tsx
+++ src/components/MoviesRow.tsx
@@ -1,6 +1,9 @@
 import { useNavigate } from 'react-router'
 import { ICONS } from '../assets'
 
+// TODO: API 연결 후 props로 데이터를 받아 화면 출력
+// TODO: image cover 추가 > 추가 후 li의 border 제거
+// TODO: image 클릭 시 detail page 이동
 function MoviesRow() {
   const navigate = useNavigate()
 
@@ -10,32 +13,27 @@ function MoviesRow() {
   }
 
   return (
-    <li className='bg-blue-400 h-[200px] w-[250px] md:w-[300px] rounded p-2 hover:scale-[1.02] group '>
+    <li className='aspect-video min-w-[250px] md:min-w-[300px] border-2 border-red-100 hover:scale-[1.04] ease-in delay-150 duration-150 group/button-hover'>
       {/* cover */}
       <div
-        className='h-full flex flex-col justify-end gap-2 cursor-pointer rounded p-4 bg-black/60 opacity-0 group-hover:opacity-100 duration-200'
+        className='h-full flex flex-col justify-end gap-2 cursor-pointer rounded p-4 bg-black/10 opacity-0 group-hover/button-hover:opacity-80 duration-200'
         onClick={handleClick}
       >
-        <h2 className='text-lg font-semibold'>영화 1</h2>
+        <h2 className='text-lg font-semibold text-red-800'>영화 a</h2>
         <div className='flex gap-2'>
-          <button
-            className='p-2 bg-red-400 rounded-full'
-            onClick={e => {
-              e.stopPropagation()
-              console.log('btn')
-            }}
-          >
-            {ICONS.play}
-          </button>
-          <button
-            className='p-2 bg-red-400 rounded-full'
-            onClick={e => {
-              e.stopPropagation()
-              console.log('btn')
-            }}
-          >
-            {ICONS.plus}
-          </button>
+          {/* 재생, 목록 추가 버튼 동작은 미 구현 */}
+          {[ICONS.play, ICONS.plus].map((icon, idx) => (
+            <button
+              key={idx}
+              className='p-2 bg-red-400 rounded-full'
+              onClick={e => {
+                e.stopPropagation()
+                console.log('btn')
+              }}
+            >
+              {icon}
+            </button>
+          ))}
         </div>
       </div>
     </li>
