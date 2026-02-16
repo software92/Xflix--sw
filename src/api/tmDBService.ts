4f991e7e417d51efbc7aa6898aed9c7cd286e157
jsw <csw9201@naver.com>
2026-02-16 22:08:30 +0900

    feat: getMovie api 함수 추가 및 Detail 페이지 연동

--- src/api/tmDBService.ts
+++ src/api/tmDBService.ts
@@ -1,5 +1,6 @@
 import { API_CONFIG, API_ENDPOINT } from '../constants/api'
 import { IApiReturn, ITrendingContents } from '../types/api'
+import { IContent, IMovie } from '../types/content'
 import { devLog } from '../utils'
 
 // 주간 인기 콘텐츠 조회 - FeaturedHero 컴포넌트 및 목록 페이지
@@ -22,6 +23,36 @@ export const getTrendingContents = async (): Promise<
     const data: ITrendingContents = await response.json()
 
     return { data, error: null }
+  } catch (error: unknown) {
+    const errorMessage =
+      error instanceof Error ? error.message : 'Unknown Error'
+
+    devLog({ message: errorMessage, type: 'error' })
+
+    return { data: null, error: errorMessage }
+  }
+}
+
+export const getMovie = async (
+  id: number | string,
+): Promise<IApiReturn<IMovie>> => {
+  try {
+    const { BASE_URL, LANGUAGE, OPTIONS } = API_CONFIG
+    const {
+      MOVIE: { DETAIL },
+    } = API_ENDPOINT
+    const response = await fetch(
+      `${BASE_URL}${DETAIL(id)}?language=${LANGUAGE}`,
+      OPTIONS,
+    )
+
+    if (!response.ok) {
+      throw new Error('현재 영화를 찾을 수 없습니다.')
+    }
+
+    const movie: IContent = await response.json()
+
+    return { data: movie, error: null }
   } catch (error: unknown) {
     const errorMessage =
       error instanceof Error ? error.message : 'Unknown Error'
--- src/pages/Detail.tsx
+++ src/pages/Detail.tsx
@@ -1,175 +1,117 @@
+import { useNavigate, useParams } from 'react-router'
 import { ICONS } from '../assets'
 import MoviesList from '../components/MoviesList'
+import { useEffect, useState } from 'react'
+import { getMovie } from '../api/tmDBService'
+import { IApiReturn } from '../types/api'
+import { Spinner } from '../components/LoadingScreen'
+import { IContent, IMovie } from '../types/content'
 
-// detail
-interface Genre {
-  id: number
-  name: string
-}
-interface SpokenLanguage {
-  english_name: string
-  iso_639_1: string
-  name: string
-}
-interface Movie {
-  adult: boolean
-  backdrop_path: string
-  genres: Genre[]
-  id: number
-  imdb_id: string
-  origin_country: string[]
-  original_language: string
-  original_title: string
-  overview: string
-  popularity: number
-  poster_path: string
-  release_date: string
-  runtime: number
-  status: string
-  title: string
-  video: boolean
-  vote_average: number
-  vote_count: number
-  spoken_languages: SpokenLanguage[]
-  tagline: string
-}
-
-// top rated movie data
-const tempMovie: Movie = {
-  adult: false,
-  backdrop_path: '/kXfqcdQKsToO0OUXHcrrNCHDBzO.jpg',
-  genres: [
-    {
-      id: 18,
-      name: 'Drama',
-    },
-    {
-      id: 80,
-      name: 'Crime',
-    },
-  ],
-  id: 278,
-  imdb_id: 'tt0111161',
-  origin_country: ['US'],
-  original_language: 'en',
-  original_title: 'The Shawshank Redemption',
-  overview:
-    'Imprisoned in the 1940s for the double murder of his wife and her lover, upstanding banker Andy Dufresne begins a new life at the Shawshank prison, where he puts his accounting skills to work for an amoral warden. During his long stretch in prison, Dufresne comes to be admired by the other inmates -- including an older prisoner named Red -- for his integrity and unquenchable sense of hope.',
-  popularity: 32.97,
-  poster_path: '/9cqNxx0GxF0bflZmeSMuL5tnGzr.jpg',
-  release_date: '1994-09-23',
-  runtime: 142,
-  spoken_languages: [
-    {
-      english_name: 'English',
-      iso_639_1: 'en',
-      name: 'English',
-    },
-  ],
-  status: 'Released',
-  tagline: 'Fear can hold you prisoner. Hope can set you free.',
-  title: 'The Shawshank Redemption',
-  video: false,
-  vote_average: 8.71,
-  vote_count: 28299,
-}
-
-const imageBaseURL = (size: string = 'original') =>
-  `https://image.tmdb.org/t/p/${size}`
-const backdropBaseURL = (size: string) => `https://image.tmdb.org/t/p/${size}/`
-
-{
-  /* <div
-          className={`bg-[url("${
-            imageBaseURL() + tempMovie.poster_path
-          }")] bg-cover bg-center bg-no-repeat h-[200px]`}
-        > */
-}
-{
-  /* floating btn - ui component*/
-}
 function FloatingButton() {
+  const navigate = useNavigate()
+
   return (
     <button
       className='fixed top-20 left-4 md:left-10 rounded-full bg-black/60 backdrop-blur-sm text-white pl-2 pr-4 py-2 flex items-center gap-2 fill-white z-20'
-      onClick={() => console.log('뒤로 가기')}
+      onClick={() => navigate(-1)}
     >
       {ICONS.leftArrow}
       <span>뒤로 가기</span>
     </button>
   )
 }
-// TODO: API 연동해서 데이터 화면에 출력
-// TODO: 뒤로 가기 버튼 클릭 시 이전 페이지 이동
 
+// [x] TODO: API 연동해서 데이터 화면에 출력
+// [x] TODO: 뒤로 가기 버튼 클릭 시 이전 페이지 이동
 function Detail() {
+  const [error, setError] = useState<IApiReturn<IContent>['error']>(null)
+  const [isLoading, setIsLoading] = useState<boolean>(true)
+  const [movie, setMovie] = useState<IApiReturn<IMovie>['data'] | IMovie>(null)
+
+  const { id } = useParams()
+
+  useEffect(() => {
+    if (!id) {
+      return
+    }
+
+    async function fetchMovie(id: string | number) {
+      const result = await getMovie(id)
+
+      setMovie(result.data)
+      setIsLoading(false)
+      setError(result.error)
+    }
+
+    fetchMovie(id)
+  }, [])
+
+  if (isLoading) {
+    return <Spinner />
+  }
+  if (!movie) {
+    throw new Error(error || '현재 영화 정보를 가져올 수 없습니다')
+  }
+
   return (
-    <>
-      <div className='relative w-full flex flex-col justify-center gap-4'>
-        <FloatingButton />
+    <article className='w-full'>
+      <FloatingButton />
+      <div className='relative flex gap-4 main-page_px'>
         <div
           className={`min-h-[80vh] w-full text-white z-10
-            flex flex-col gap-6 justify-end pb-16 md:pb-32
-            main-page_px`}
+            flex flex-col gap-6 justify-end pb-16 md:pb-32`}
         >
-          <h1 className='font-light text-4xl md:text-6xl'>
-            {tempMovie.original_title}
-          </h1>
-          {/* row */}
+          <h1 className='font-light text-4xl md:text-6xl'>{movie.title}</h1>
+          <div className='flex gap-2'>
+            {movie.genres.map(genre => (
+              <div
+                key={genre.id}
+                className='px-3 py-1 bg-white rounded-lg'
+              >
+                <span className='text-black/80 font-bold text-lg'>
+                  {genre.name}
+                </span>
+              </div>
+            ))}
+          </div>
+
           <div className='flex gap-4 text-base md:text-lg'>
-            <span>{tempMovie.release_date}</span>
+            <span>{movie.release_date}</span>
             <span>
-              {Math.floor(tempMovie.runtime / 60)}h {tempMovie.runtime % 60}m
+              {Math.floor(movie.runtime / 60)}시간 {movie.runtime % 60}분
             </span>
           </div>
-          <p className='line-clamp-2 text-base md:text-lg'>
-            {tempMovie.overview}
-          </p>
+
+          <p className='line-clamp-2 text-base md:text-lg'>{movie.overview}</p>
           <div className='flex gap-3'>
-            <button className='px-3 md:px-5 py-3 flex gap-2 items-center rounded-md bg-gray-200 text-black hover:bg-gray-200/95 text-sm'>
+            <button className='px-3 md:px-4 py-4 flex gap-2 items-center rounded-md bg-gray-200 text-black hover:bg-gray-200/95 text-sm'>
               {ICONS.play}
-              <span>재생</span>
+              <span className='text-lg font-semibold'>재생</span>
             </button>
-            <button className='px-3 md:px-5 py-3 flex gap-2 items-center rounded-md hover:bg-white/30 bg-gray-300/25 text-white backdrop-blur-md text-sm'>
+            <button className='px-3 md:px-5 py-4 flex gap-2 items-center rounded-md hover:bg-white/30 bg-gray-300/25 text-white backdrop-blur-md text-sm'>
               {ICONS.plus}
-              <span>상세 정보 버튼</span>
+              <span className='text-lg font-semibold'>상세 정보 버튼</span>
             </button>
           </div>
           <div className='absolute -bottom-6 left-[50%] translate-x-[-50%] animate-tongtong'>
             {ICONS.chevronDown}
           </div>
         </div>
-        <div className='absolute inset-0 bg-[url("https://images.unsplash.com/photo-1600333791066-f3c7e752b44e?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=M3w3Nzg4Nzd8MHwxfHNlYXJjaHwxfHxtb3ZpZSUyMGFjdGlvbiUyMGhlcm98ZW58MXx8fHwxNzY4NDU0ODc2fDA&ixlib=rb-4.1.0&q=80&w=1080")] bg-cover bg-center bg-no-repeat' />
-        <div className='absolute inset-0 bg-gradient-to-r from-black/70 to-black/0' />
-        <div className='absolute inset-0 bg-gradient-to-t from-black via-black/10 to-transparent' />
-      </div>
-      <div
-        className='flex flex-col md:grid md:grid-cols-3 gap-16 py-8
-      text-white/80 main-page_px'
-      >
-        <div className='md:col-span-2 flex flex-col gap-4'>
-          <h3 className='text-lg text-white/90'>줄거리</h3>
-          <p className=''>{tempMovie.overview}</p>
-        </div>
-        <div className='md:col-span-1 flex flex-col gap-3 text-sm'>
-          <div className='flex gap-2 items-center'>
-            <h4 className='text-sm text-gray-400/80'>출연:</h4>
-            <p className=''>가나다라</p>
-          </div>
-          <div className='flex gap-2 items-center'>
-            <h4 className='text-sm text-gray-400/80'>감독:</h4>
-            <p className=''>가나다라</p>
-          </div>
-          <div className='flex gap-2 items-center'>
-            <h4 className='text-sm text-gray-400/80'>장르:</h4>
-            <p className=''>가나다라</p>
-          </div>
+        <div className='absolute inset-0'>
+          <img
+            src={`https://image.tmdb.org/t/p/original/${movie.backdrop_path}`}
+            alt={`${movie.title} 포스터` || '영화 포스터'}
+            className='w-full h-full object-cover'
+          />
+
+          <div className='absolute inset-0 bg-gradient-to-r from-black/70 to-black/0' />
+          <div className='absolute inset-0 bg-gradient-to-t from-black via-black/10 to-transparent' />
         </div>
       </div>
 
       <MoviesList title='인기 있는' />
       <MoviesList title='액션' />
-    </>
+    </article>
   )
 }
 
