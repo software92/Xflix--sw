c2190cb8538ace4359203dd08cb8e1d8b0f718e9
jsw <csw9201@naver.com>
2026-02-19 19:20:46 +0900

    feat: ContentsList 슬라이드 버튼 로직 추가
    - 리스트에 스크롤 이벤트를 사용해 좌우 끝에서 버튼 숨기기
    - 버튼 클릭 이벤트를 사용해 슬라이드 좌우 이동(scrollBy)

--- src/components/ContentsList.tsx
+++ src/components/ContentsList.tsx
@@ -5,21 +5,48 @@ import { IContent } from '../types/content'
 import { Spinner } from './LoadingScreen'
 import ContentRow from './ContentRow'
 import { MediaType } from '../types/common'
+import { useRef, useState } from 'react'
 
 interface IContentsList {
   title: string
   category: MediaType
   apiPath: ApiPath
 }
+type scrollDirection = 'LEFT' | 'RIGHT'
 
 // [o] TODO: 슬라이더 형태로 변경, 버튼 클릭 시 좌우 이동(+애니메이션), 버튼을 screen상 좌우에 고정
 // [o] TODO: h2>title text 크기 조정
 // [o] TODO: API 연동으로 슬라이드 카드에 데이터 뿌리기
-
-// TODO: 슬라이드의 왼쪽 오른쪽 끝에서 버튼 비활성화
-// TODO: 버튼을 클릭해서 슬라이드를 이동
+// [o] TODO: 슬라이드의 왼쪽 오른쪽 끝에서 버튼 비활성화
+// [o] TODO: 버튼을 클릭해서 슬라이드를 이동
 function ContentsList({ title, category, apiPath }: IContentsList) {
   const { isLoading, error, contents } = useGetMovies(apiPath)
+
+  const [isStart, setIsStart] = useState(true)
+  const [isEnd, setIsEnd] = useState(false)
+
+  const scrollRef = useRef<HTMLUListElement>(null)
+
+  function moveScroll(direction: scrollDirection) {
+    if (scrollRef?.current) {
+      const moveAmount = scrollRef.current.clientWidth / 2
+      const moving = direction === 'LEFT' ? -moveAmount : moveAmount
+
+      scrollRef.current.scrollBy({
+        left: moving,
+        behavior: 'smooth',
+      })
+    }
+  }
+
+  function handleScroll() {
+    if (scrollRef?.current) {
+      const { scrollLeft, scrollWidth, clientWidth } = scrollRef.current
+
+      setIsStart(scrollLeft === 0 ? true : false)
+      setIsEnd(scrollLeft + clientWidth === scrollWidth ? true : false)
+    }
+  }
 
   if (isLoading) {
     return (
@@ -38,7 +65,7 @@ function ContentsList({ title, category, apiPath }: IContentsList) {
     throw new Error(error || '현재 컨텐츠 정보를 가져올 수 없습니다')
   }
 
-  const contentsList: IContent[] = contents.results
+  const contentsList: IContent[] = contents.results.slice(0, 6)
 
   return (
     <div
@@ -47,13 +74,27 @@ function ContentsList({ title, category, apiPath }: IContentsList) {
     >
       <h2 className='text-2xl font-bold'>{title}</h2>
       <div className='relative h-full group'>
-        <button className='absolute top-0 bottom-0 left-0 w-10 bg-gray-700/100 justify-center items-center z-[11] hidden group-hover:flex hover:scale-150'>
-          <span>{ICONS.leftArrow}</span>
-        </button>
-        <button className='absolute top-0 bottom-0 right-0 w-10 bg-gray-700/100 justify-center items-center z-[11] hidden group-hover:flex hover:scale-150'>
-          <span>{ICONS.rightArrow}</span>
-        </button>
-        <ul className='flex overflow-x-scroll scrollbar-hide gap-2 h-full'>
+        {!isStart && (
+          <button
+            className='absolute top-0 bottom-0 left-0 w-10 bg-black/50 justify-center items-center z-[11] hidden group-hover:flex'
+            onClick={() => moveScroll('LEFT')}
+          >
+            <span>{ICONS.leftArrow}</span>
+          </button>
+        )}
+        {!isEnd && (
+          <button
+            className='absolute top-0 bottom-0 right-0 w-10 bg-black/50 justify-center items-center z-[11] hidden group-hover:flex'
+            onClick={() => moveScroll('RIGHT')}
+          >
+            <span>{ICONS.rightArrow}</span>
+          </button>
+        )}
+        <ul
+          className='flex overflow-x-scroll scrollbar-hide gap-2 h-full'
+          ref={scrollRef}
+          onScroll={handleScroll}
+        >
           {contentsList.map(content => (
             <ContentRow
               key={content.id}
